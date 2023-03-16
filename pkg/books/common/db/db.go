5f9ec139d2782aa47ca69a4866f0fb4b9e1689bc
LeoAntunesBrombilla <antunes.b.leonardo@gmail.com>
2023-03-15 22:47:19 -0300

    project setup

---
+++ pkg/books/common/db/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+
+  "log"
+
+
+  "github.com/LeoAntunesBrombilla/go-gin-postgresql-api/pkg/common/models"
+  "gorm.io/driver/postgres"
+  "gorm.io/gorm"
+)
+
+
+func Init(url string) *gorm.DB {
+  db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  db.AutoMigrate(&models.Book{})
+
+  return db
+}
